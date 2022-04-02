576ed306cdab1edb1fb1eff8e5dee99b63189817
Vinayak-07 <75664275+Vinayak-07@users.noreply.github.com>
2022-04-02 14:58:55 +0530

    Add files via upload

---
+++ graph.py
@@ -0,0 +1,7 @@
+from turtle import color
+import pandas as pd
+import plotly.express as px
+
+df = pd.read_csv("data.csv")
+figure = px.line(df,x="date", y = "cases",color  = "country",title = 'covid data')
+figure.show()
